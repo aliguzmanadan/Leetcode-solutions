5f38d576a975cbf28aeecfda25b891af89211498
aliguzmanadan <64287655+aliguzmanadan@users.noreply.github.com>
2022-02-14 16:13:58 +0100

    Time: 2207 ms (67.92%), Space: 106.3 MB (75.65%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,4 +1,5 @@
 class Solution:
+    #Create adjancecy list with the list of all edges
     def adjacencyList(self, n, edges):
             adjancencyList = [[] for i in range(n)]
             for a, b in edges:
@@ -6,22 +7,28 @@ class Solution:
                 adjancencyList[b].append(a)
 
             return adjancencyList
-
-    def validPathRecursive(self, adjList, source, destination, visited=set()):
-        visited.add(source)
-        if source == destination:
-            return True
-
-        for neighbor in adjList[source]:
-            if neighbor not in visited and self.validPathRecursive(adjList, neighbor, destination, visited):
-                return True
-
-        return False
-
     
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        adjList = self.adjacencyList(n, edges)
-        return self.validPathRecursive(adjList, source, destination, visited = set())
+    #Iterative Approach
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int, ) -> bool:
         
+        ajdList = self.adjacencyList(n, edges)
         
+        visited = set()
+        stack = [source]
+        
+        while stack:
+            #pop top element from stack and process it
+            node = stack.pop()
+            if node == destination:
+                return True
+            
+            
+            visited.add(node)
+            
+            for neighbor in ajdList[node]:
+                if neighbor not in visited:
+                    stack.append(neighbor)
+                    
+        return False
+            
         
