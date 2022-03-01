5614a316669fed7cf527e958d482e03bda40cb70
aliguzmanadan <64287655+aliguzmanadan@users.noreply.github.com>
2022-03-01 17:07:54 +0100

    Time: 103 ms (84.47%), Space: 15.9 MB (22.81%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,22 +1,23 @@
+from collections import deque
+
 class Solution:
-    
-    #Recursive function that, given a start A and destination B,
-    #returns the list of all paths from A to B
-    def allPathsSourceTargeRecursive(self, graph, start, destination):
-        
-        #Base case: If start == destination then there is only one path 
-        if start == destination:
-            return[[start]]
-        
-        #Otherwise look for paths starting in the neighborhs of start
-        paths = []
-        for neighbor in graph[start]:
-            paths += [[start] + p for p in self.allPathsSourceTargeRecursive(graph, neighbor, destination)]
-        
-        return paths
-            
-            
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        return self.allPathsSourceTargeRecursive(graph, 0, len(graph)-1)
-        
+        #initialize paths, queue, n, (we dont need to keep track of visited vertices since its is a acyclic graph!)
+        paths = []
+        n = len(graph)
+        queue = deque([[0]])
+
+        while queue:
+
+            #Pop first partial path available and check if it is completed
+            path = queue.popleft()
+            if path[-1] == n-1:
+                paths.append(path)
+
+            #if not the go to the next levels
+            else:
+                for neighbor in graph[path[-1]]:
+                        queue.append(path+[neighbor])
+
+        return paths
         
